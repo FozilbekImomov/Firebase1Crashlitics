fdb1b5729a31a17ce446e4b9d2445cfc15be16f3
Fozilbek Imomov <baradaw467@vvaa1.com>
2021-07-16 07:13:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/firebase1/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.firebase1
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Firebase.messaging.isAutoInitEnabled = true
+    }
+
+
+    fun TestCrash(view: View) {
+        FirebaseCrashlytics.getInstance().setCustomKey("str_key", "hello")
+
+        throw RuntimeException("Test Crash")
+
+    }
+}
